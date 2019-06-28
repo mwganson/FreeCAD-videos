7dfa2d92519c892ba0e6c69ef6503c14353088fc
mwganson <mwganson@gmail.com>
2019-06-28 17:14:30 -0500

    modify make_url.py so it rewrites itself.  this way it always appears at the top of the folder list when sorted by date modified

--- make_url.py
+++ make_url.py
@@ -7,6 +7,15 @@ this file, which then creates the url and video tags string, and then copies it 
 This greatly improves things because now I don't have to go to github.com each time to get the url.
 """
 
+def modifyThineSelf():
+    """This is just to modify the file so it appears at the top of the folder when sorting by modified date."""
+    fin = open(__file__, 'r')
+    code = fin.read()
+    fin.close()
+    fout = open(__file__, 'w')
+    fout.write(code)
+    fout.close()
+
 def copy2clip(txt):
     cmd='echo '+txt.strip()+'|clip'
     return subprocess.check_call(cmd, shell=True)
@@ -16,6 +25,7 @@ prepend = "https://raw.githubusercontent.com/mwganson/FreeCAD-videos/master/"
 if len(sys.argv) != 2:
     input("Please drag and drop a file or enter filename as command line parameter")
 else:
+    modifyThineSelf()
     fileName = os.path.basename(sys.argv[1])
     data = "[video]" + prepend + fileName + "[/video]"
     copy2clip(data)
